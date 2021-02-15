8b2a339ec0b97429d9c0379e1e3efd49d5827621
Dennis <58036609+dennight@users.noreply.github.com>
2021-02-15 12:47:09 +0200

    Delete app directory

--- app/js/app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-let menuButton = document.querySelector('.brain-menu-button');
-let nav = document.querySelector('.brain-nav-list');
-let header = document.querySelector('.brain-header')
-menuButton.addEventListener("click", function () {
-    header.classList.toggle("is-open");
-    nav.classList.toggle("is-open");
-});
